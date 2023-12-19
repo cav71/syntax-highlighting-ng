a1e23e48be20a9c6a41899e83e217cc906abe362
antonio <a.cavallo@cavallinux.eu>
2023-12-19 10:18:28 +0100

    add assets fixture to conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,28 @@
 from __future__ import annotations
 import sys
 import types
-
+from pathlib import Path
 import pytest
+
+@pytest.fixture(scope="function")
+def assets(request):
+
+    class Asset:
+        def __init__(self):
+            self.basedir = Path(__file__).parent
+        def lookup(self, path: Path) -> Path|None:
+            candidates = [
+                f"assets/{request.node.module.__name__}",
+                "assets",
+            ]
+            for candidate in candidates:
+                if (target := (self.basedir / candidate / path)).exists():
+                    return target
+        def read_text(self, path: Path) -> str|None:
+            if (path:=self.lookup(path)):
+                return path.read_text()
+
+    return Asset()
 
 
 @pytest.fixture(scope="function")
@@ -17,12 +37,32 @@ def fake_anki21(monkeypatch):
         def point_version(self):
             return 231001
 
-    name = "anki"
-    old = sys.modules.get(name)
-    sys.modules[name] = Anki(name)
+
+    class AnkiHooks(types.ModuleType):
+        def addHook(self):
+            return 999
+
+    class Aqt(types.ModuleType):
+        pass
+
+    class AqtMV(types.ModuleType):
+        def callme(self):
+            return 123
+
+    replacements = [
+        ("anki", Anki),
+        ("anki.hooks", AnkiHooks),
+        ("aqt", Aqt),
+        ("aqt.mw", AqtMV),
+    ]
+    old = {}
+    for name, cls in replacements:
+        old[name] = sys.modules.get(name)
+        sys.modules[name] = cls(name)
     yield old
-    if old:
-        sys.modules[name] = old
-    else:
-        del sys.modules[name]
-    
+    for name, mod in reversed(old.items()):
+        if mod:
+            sys.modules[name] = mod
+        else:
+            del sys.modules[name]
+
