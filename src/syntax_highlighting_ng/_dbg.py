425a7557e7aa06fa454c863cd842d9af72869433
antonio <a.cavallo@cavallinux.eu>
2023-12-25 07:48:46 +0100

    cleanup

--- src/syntax_highlighting_ng/_dbg.py
+++
@@ -1,8 +0,0 @@
-## INTERNAL MODULE (DEBUG)
-def set_trace():
-    import sys
-
-    sys.path.insert(0, "/Users/antonio/shared/pydevd-2.10.0")
-    import pydevd
-
-    pydevd.settrace("localhost", port=12345, stdoutToServer=True, stderrToServer=True)
