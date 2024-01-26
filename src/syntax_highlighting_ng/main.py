183ebc165a8a6d519c7116b9479db4bb0c268021
antonio <a.cavallo@cavallinux.eu>
2024-01-26 14:23:26 +0100

    fixes

--- src/syntax_highlighting_ng/main.py
+++ src/syntax_highlighting_ng/main.py
@@ -55,7 +55,7 @@ LIMITED_LANGS = config.local_conf["limitToLangs"]
 #                        to show the user AND
 #  The "language aliases": short, cryptic names for internal
 #                          use by HtmlFormatter
-LANGUAGES_MAP = {lex[0]: lex[1][0] for lex in get_all_lexers() if len(lex) > 1}
+LANGUAGES_MAP = {lex[0]: lex[1][0] for lex in get_all_lexers() if lex[1]}
 
 
 # Misc
