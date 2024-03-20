82e96fb02d6bb5a29c4a7eb409fa7ddc8bba0240
Antonio Cavallo <a.cavallo@cavallinux.eu>
2024-03-19 23:11:21 -0400

    improve make.py

--- make.py
+++ make.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
+"""make.py file
+
+This is the same as a `make` file, but in python.
+"""
 from __future__ import annotations
 import argparse
+import inspect
 import sys
 import json
 import logging
+import platform
 import subprocess
 from pathlib import Path
 
@@ -28,34 +34,45 @@ def error(msg, code=1):
     if code:
         sys.exit(code)
 
-def task_e2e(_):
-    workdir = "/app"
-    # docker buildx build -t boo .
-    # docker run --rm -ti -v %CD%:/app boo /venv/bin/python /app/make.py
 
-    # subprocess.check_call([
-    #     "python", "-m", "venv", "/app/venv",
-    # ], cwd=workdir)
-    # subprocess.check_call([
-    #     "/app/venv/bin/python", "-m", "pip", "install", "-r", "requirements.txt",
-    # ], cwd=workdir)
+def task_info():
+    print(f"sys.executable: {sys.executable}")
+    print(f"sys.platform: {sys.platform}")
+    print(f"platform.uname().system: {platform.uname().system}")
+    print(f"platform.uname().machine: {platform.uname().machine}")
+    print(f"platform.uname().release: {platform.uname().release}")
+
+
+def task_e2e(_):
     try:
-        subprocess.check_call([
-            f"{VENV}/bin/aab", "build", "-t", "anki21", "-d", "local", "current",
-        ], cwd=APPDIR)
-    except:
+        subprocess.check_call(
+            [
+                f"{VENV}/bin/aab",
+                "build",
+                "-t",
+                "anki21",
+                "-d",
+                "local",
+                "current",
+            ],
+            cwd=APPDIR,
+        )
+    except Exception:
         pass
 
     for p in sorted(Path(APPDIR).rglob("*")):
         print(f"| {p}")
 
 
-def task_build_unix(args):
-    "build on windows using docker"
+def task_cross_build(args):
+    "cross build an anki plugin using docker"
     if sys.platform != "win32":
         error(f"must run on windows (found '{sys.platform}')")
+
     p = argparse.ArgumentParser()
+    p.add_argument("-n", "--info", help="run the info task", action="store_true")
     p.add_argument("--rm", help="rebuild image from scratch", action="store_true")
+
     options = p.parse_args(args)
 
     if options.rm:
@@ -66,15 +83,25 @@ def task_build_unix(args):
         log.info("building image '%s'", IMAGE)
         run(["docker", "buildx", "build", "-t", IMAGE, "."])
 
-    subprocess.check_call([
-        "docker", "run", "-v", f"{str(Path.cwd())}:/app", "--rm", "-ti",
-        IMAGE,
-        "python", f"{APPDIR}/make.py", "e2e"
-    ])
+    app = "info" if options.info else "e2e"
+    subprocess.check_call(
+        [
+            "docker",
+            "run",
+            "-v",
+            f"{str(Path.cwd())}:/app",
+            "--rm",
+            "-ti",
+            IMAGE,
+            "python",
+            f"{APPDIR}/make.py",
+            app,
+        ]
+    )
 
 
 COMMANDS = {
-    name[len("task_"):].replace("_", "-"): fn
+    name[len("task_") :].replace("_", "-"): fn
     for name, fn in locals().items()
     if name.startswith("task_")
 }
@@ -105,4 +132,14 @@ Running on: {sys.platform}
         sys.exit()
 
     workdir = Path(__file__).parent
-    COMMANDS[sys.argv[1]](sys.argv[2:]) #Repo(workdir))
+    if sys.argv[1] not in COMMANDS:
+        error(f"cannot find the command '{sys.argv[1]}' in: {','.join(COMMANDS)}")
+
+    function = COMMANDS[sys.argv[1]]
+    n = len(inspect.signature(function).parameters)
+    if n == 1:
+        function(sys.argv[2:])
+    elif n == 0:
+        function()
+    else:
+        function(*sys.argv[2:])
